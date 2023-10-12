b422c745fab4587ceb5eab68b129241a0a8bdd26
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-10-13 00:36:14 +0530

    Create groupRouter.js

---
+++ router/groupRouter.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const groupController = require("../controller/groupController");
+const { authUser } = require("../middleware/authMiddleware");
+const { isGroupAdmin } = require("../middleware/groupAdminMiddleware");
+const { isGroupMember } = require("../middleware/groupMemberMiddleware");
+
+const router = express.Router();
+
+router.get("/", authUser, groupController.getUserAllGroups);
+
+router.post("/create-group", authUser, groupController.createGroup);
+
+router.post("/add-users", authUser, groupController.addUsersToGroup);
+
+router.get(
+  "/:groupId",
+  authUser,
+  isGroupMember,
+  groupController.getSingleGroup
+);
+
+router.delete(
+  "/:groupId/:userId",
+  authUser,
+  isGroupMember,
+  isGroupAdmin,
+  groupController.deleteMemberFromGroup
+);
+
+router.delete(
+  "/:groupId",
+  authUser,
+  isGroupMember,
+  isGroupAdmin,
+  groupController.deleteGroup
+);
+
+router.get(
+  "/all-users/:groupId",
+  authUser,
+  isGroupMember,
+  isGroupAdmin,
+  groupController.getUsersNotInThisGroup
+);
+
+router.put(
+  "/:groupId/:userId",
+  authUser,
+  isGroupMember,
+  isGroupAdmin,
+  groupController.makeNewAdminToTheGroup
+);
+
+module.exports = router;
